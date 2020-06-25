55de9d4c6d9e78204b246eb7d00a6af8f20af80b
kanga333 <e411z7t40w@gmail.com>
2020-06-25 10:23:34 +0900

    Add ParameterMapList

--- src/map.ts
+++ src/map.ts
@@ -15,3 +15,32 @@ class ParameterMap {
         return null
     }
 }
+
+class ParameterMapList {
+    prams: ParameterMap[];
+
+    constructor(raw_json: string) {
+        let ps = new Array<ParameterMap>()
+        let parsed = JSON.parse(raw_json)
+        //TODO: validation
+        for (let key in parsed) {
+            let values = new Map<string, string>()
+            for (let val in parsed[key]) {
+                values.set(val, parsed[key][val]);
+            }
+            let p = new ParameterMap(key, values)
+            ps.push(p)
+        }
+        this.prams = ps
+    }
+
+    match(key: string): Map<string, string> | null {
+        this.prams.forEach(param => {
+            let matched = param.match(key)
+            if ( matched instanceof Map) {
+                return matched
+            }
+        })
+        return null
+    }
+}
