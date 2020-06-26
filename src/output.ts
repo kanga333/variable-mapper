4471a3815ceea94fc55e8710d3231bd69c8705e6
kanga333 <e411z7t40w@gmail.com>
2020-06-27 00:33:17 +0900

    Fix for

--- src/output.ts
+++ src/output.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 
 export function logOutput(input: Map<string, string>): void {
-  for (const key in input.keys()) {
-    core.info(`${key}: ${input.get(key)}`)
+  for (const item of input.entries()) {
+    core.info(`${item[0]}: ${item[1]}`)
   }
 }
